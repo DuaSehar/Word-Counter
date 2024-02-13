504788bd14345d2c8d8d8beff61d02863bc75bd9
DuaSehar <duaasehr@gmail.com>
2024-02-13 23:46:05 +0500

    code added

---
+++ index.ts
@@ -0,0 +1,28 @@
+import inquirer from 'inquirer';
+
+async function wordCounter() {
+    const questions = [
+        {
+            type: 'input',
+            name: 'paragraph',
+            message: 'Enter a paragraph:'
+        }
+    ];
+
+    const { paragraph } = await inquirer.prompt(questions);
+
+    const countWordsAndCharacters = (text: string) => {
+        const words = text.split(/\s+/).filter(word => word !== '');
+        const wordCount = words.length;
+        const characterCount = words.join('').length;
+        return { wordCount, characterCount };
+    };
+
+   const { wordCount, characterCount } = countWordsAndCharacters(paragraph);
+
+    console.log(`Character count (without whitespace): ${characterCount}`);
+    console.log(`Word count: ${wordCount}`);
+}
+
+// Call the function to start the word counter
+wordCounter();
